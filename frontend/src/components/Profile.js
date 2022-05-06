faef746136614d3623a36519e2327a3347b2f898
stavroskostopoulos <77120544+stavroskostopoulos@users.noreply.github.com>
2022-05-06 19:13:20 +0300

    profile in the making 2

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import Button from '@mui/material/Button';
 
 import "../css/profile.css"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import EditIcon from '@mui/icons-material/Edit';
 
+const useStyles = makeStyles({
+    profileTextField: {
+        // padding: '10px!important'
+        
+    }
+
+});
 
 function Profile() {
   
+    const classes = useStyles();
+
+    const [edit, setEdit] = React.useState(true);
 
     return (
       
@@ -17,17 +32,68 @@ function Profile() {
 
 
                 <div className="profile-container">
-                <div className="profile-column-left" >
-                    
-                    <img className="profile-picture" src="/faithplusone.jpg"></img>
-                </div>
-                <div className="profile-column-right">
-                    <h2 className="name-title">Stavros Kostopoulos</h2>
-                </div>
+                    <div className="profile-column-left" >
+                        <img className="profile-picture" src="/faithplusone.jpg"></img>
+                    </div>
+                    <div className="profile-column-right">
+                        <h2 className="name-title">Stavros Kostopoulos</h2>
+                        
+                        <div className="profile-info-container">
+                            <div className="left-profile-info-column">
+
+                                {!edit &&
+                                    <TextField
+                                        disabled={edit}
+                                        id="outlined-disabled"
+                                        label="First name"
+                                        defaultValue="Stavros"
+                                        className="profileinfo-tf"
+                                        sx={{ mt: 5}} 
+                                    />
+                                }
+                                <TextField
+                                    disabled={edit}
+                                    id="outlined-disabled"
+                                    label="Email"
+                                    defaultValue="sdi1700068@di.uoa.gr"
+                                    className="profileinfo-tf" 
+                                    sx={{ mt: 5}}
+                                />
+                            </div>
+                            <div className="right-profile-info-column">
+                                {!edit &&
+
+                                    <TextField
+                                        disabled={edit}
+                                        id="outlined-disabled"
+                                        label="Last name"
+                                        defaultValue="Kostopoulos"
+                                        className="profileinfo-tf" 
+                                        sx={{ mt: 5}}  
+                                    />
+                                
+                                }
+
+                                <TextField
+                                    disabled={edit}
+                                    id="outlined-disabled"
+                                    label="Type"
+                                    defaultValue="Seller"
+                                    className="profileinfo-tf"
+                                    sx={{ mt: 5}} 
+                                    />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="profile-buttons">
+                        <Button variant="contained" endIcon={<EditIcon/>} className="edit-btn" sx={{ mt: 2}} onClick={() => setEdit(false)}>
+                            Edit
+                        </Button>
+                    </div>
                 </div> 
             
-
-
+                
+                
 
 
             </div>
