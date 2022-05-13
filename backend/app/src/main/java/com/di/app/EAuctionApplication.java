08c6f0a54388da7ae6ddad67c87b3b512007d009
vaspio <billpas@protonmail.com>
2022-05-13 17:35:15 +0300

    HTTPS/SSL implemented

--- backend/app/src/main/java/com/di/app/EAuctionApplication.java
+++ backend/app/src/main/java/com/di/app/EAuctionApplication.java
@@ -1,8 +1,15 @@
 package com.di.app;
 
 import com.di.app.user.User;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -15,5 +22,36 @@ public class EAuctionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EAuctionApplication.class, args);
 
+	}
+
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// Enable SSL Traffic
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		return tomcat;
+	}
+
+
+    // Redirect from HTTP 8080 to HTTPS 8443
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 }
