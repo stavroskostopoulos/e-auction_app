14a51b8617a74e571dd4fb03d013086c9840c74c
kostopez <stavroskostopoulos@outlook.com>
2022-10-02 23:16:12 +0300

    screenshots added

--- frontend_demo/src/variables/categories.js
+++
@@ -1,9 +0,0 @@
-const productCategories = [ 
-    'Used', 
-    'Unused', 
-    'Electronics', 
-    'Fashion', 
-    'Health & Beauty' 
-];
-
-export default productCategories;
--- frontend_demo/src/variables/durations.js
+++
@@ -1,5 +0,0 @@
-const possibleDuration = [ 
-    ...Array(57).keys() 
-].map(((value) => { return value+4; }));
-
-export default possibleDuration;
